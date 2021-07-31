2a64ee5b645695f3c402ef74366cc756790a086f
TatianaChepovaya <taniacheport@gmail.com>
2021-07-31 17:03:12 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,21 @@
+//ВАРИАНТ 1
+
+// const categoriesEl = document.querySelectorAll('li.item');
+// console.log(`В списке ${categoriesEl.length} категории:`);
+// const titleEl = document.querySelectorAll('h2');
+// titleEl.forEach(name => {
+//   console.log(`Категория: ${name.textContent}`);
+//   console.log(
+//     `Количество элементов: ${name.parentNode.querySelectorAll('li').length}`
+//   );
+// });
+
+//ВАРИАНТ 2
+
+const categoriesEl = document.querySelectorAll('li.item');
+console.log(`В списке ${categoriesEl.length} категории:`);
+categoriesEl.forEach(item =>{
+    console.log(`Категория: ${item.querySelector('h2').textContent}`);
+    console.log(`Количество элементов: ${item.querySelectorAll('li').length}`)
+}
+);
